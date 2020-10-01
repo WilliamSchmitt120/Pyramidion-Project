107f3df64eeda8606cb517349fbf2a86192c55b9
William <w.schmitt@rubika-edu.com>
2020-10-01 18:58:00 +0200

    SetUp Camera

---
+++ Pyramidion Project/Assets/Dossiers Persos/WIS/WIS_Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIS_Rotate : MonoBehaviour
+{
+
+    public GameObject cube1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cube1.transform.Rotate(new Vector3(1, 0, 0));
+
+
+
+
+    }
+}
