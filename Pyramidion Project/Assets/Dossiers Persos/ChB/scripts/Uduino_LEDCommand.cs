857f153e10fa5644086dc9b00bb3ef89ed60ae5c
Charles BENTEIN <c.bentein@rubika-edu.com>
2020-11-12 15:03:46 +0100

    LED commands with enum

---
+++ Pyramidion Project/Assets/Dossiers Persos/ChB/scripts/Uduino_LEDCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class Uduino_LEDCommand : MonoBehaviour
+{
+    public enum lightCommand { Reset, Victory, Defeat};
+    bool inLightSequence = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inLightSequence)
+        {
+            if (WIS_MainManager.instance.ledBlue)
+            {
+                CommandLED(lightCommand.Victory);
+                inLightSequence = true;
+            }
+            else if (WIS_MainManager.instance.ledRed)
+            {
+                CommandLED(lightCommand.Defeat);
+                inLightSequence = true;
+            }
+        }
+    }
+    
+    /// <summary>
+    /// CHB -- Select command with lightCommand enum members Victory, Defeat or Reset
+    /// </summary>
+    /// <param name="command"></param>
+    public void CommandLED(lightCommand command)
+    {
+        switch (command)
+        {
+            case lightCommand.Victory:
+                UduinoManager.Instance.sendCommand("victory");
+                break;
+
+            case lightCommand.Defeat:
+                UduinoManager.Instance.sendCommand("defeat");
+                break;
+
+            case lightCommand.Reset:
+                UduinoManager.Instance.sendCommand("reset");
+                inLightSequence = false;
+                break;
+        }
+    }
+}
